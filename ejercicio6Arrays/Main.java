1d8a392ec4705036ab560099e5ed396e72e0582e
Alejandro <akexgarcia47@gmail.com>
2025-06-23 17:52:02 +0200

    Modificación del ejercicio 6

--- ejercicio6Arrays/Main.java
+++ ejercicio6Arrays/Main.java
@@ -1,48 +1,15 @@
 package inicio.ejercicio6Arrays;
 
 public class Main {
-    public static int indice = 0;
-    public static Paciente[] pacientes = new Paciente[10];
-
-    public static void insertarPaciente(Paciente paciente){
-        pacientes[indice] = paciente;
-        indice++;
-    }
-
-    public static Paciente buscarPaciente(int numHistorial){
-        Paciente res = null;
-        for (Paciente p : pacientes){
-            if (p.getNroHistorial() == numHistorial){
-                return p;
-            }
-        }
-        return res;
-    }
-
-    public static void mostrarPacientesMayoresDe65(){
-        for (int i = 0; i < pacientes.length; i++) {
-            if (pacientes[i] != null && pacientes[i].getEdad() > 65){
-                System.out.println(pacientes[i].imprimir());
-            }
-        }
-    }
-
-    public static void mostrarPacientes(){
-        for (int i = 0; i < pacientes.length; i++) {
-            if (pacientes[i] != null){
-                System.out.println(pacientes[i].imprimir());
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        insertarPaciente(new Paciente("Pedro", 70, 1));
-        insertarPaciente(new Paciente("Nerea", 26, 2));
-        insertarPaciente(new Paciente("Alex", 20, 3));
-        mostrarPacientes();
+        Salud salud = new Salud();
+        salud.insertarPaciente(new Paciente("Pedro", 70, 1));
+        salud.insertarPaciente(new Paciente("Nerea", 26, 2));
+        salud.insertarPaciente(new Paciente("Alex", 20, 3));
+        salud.mostrarPacientes();
         System.out.println("Buscamos el paciente 2: ");
-        System.out.println(buscarPaciente(2).imprimir());
+        System.out.println(salud.buscarPaciente(2).imprimir());
         System.out.println("Imprimimos mayor de 65: ");
-        mostrarPacientesMayoresDe65();
+        salud.mostrarPacientesMayorDe65();
     }
 }
--- ejercicio6Arrays/Salud.java
+++ ejercicio6Arrays/Salud.java
@@ -1,4 +1,53 @@
 package inicio.ejercicio6Arrays;
 
 public class Salud {
+    private Paciente[] pacientes;
+    private int indice;
+    private static final int CAPACIDAD = 10;
+
+    public Salud(){
+        this.pacientes = new Paciente[CAPACIDAD];
+        this.indice = 0;
+    }
+
+    public Salud(int tamano){
+        this.pacientes = new Paciente[tamano];
+        this.indice = 0;
+    }
+
+    public void insertarPaciente(Paciente paciente){
+        pacientes[indice] = paciente;
+        indice++;
+    }
+
+    public Paciente buscarPaciente(int numeroPac){
+        Paciente res = null;
+        for (int i = 0; i < indice; i++) {
+            Paciente p = pacientes[i];
+            if (p.getNroHistorial() == numeroPac){
+                return p;
+            }
+        }
+        return res;
+    }
+
+    public void mostrarPacientesMayorDe65(){
+        int contador = 0;
+        for (int i = 0; i < indice; i++) {
+            Paciente p = pacientes[i];
+            if (p.getEdad() > 65) {
+                System.out.println(p.imprimir());
+                contador++;
+            }
+        }
+        if (contador == 0){
+            System.out.println("No hay pacientes con esa edad.");
+        }
+    }
+
+    public void mostrarPacientes(){
+        for (int i = 0; i < indice; i++) {
+            System.out.println(pacientes[i].imprimir());
+        }
+    }
 }
